0599df25e86b6af33dc9218237ac10d02639587c
bernievcooke-cloud <bernievcooke@gmail.com>
2026-03-02 14:25:06 +1100

    Add trip planner report handler in core/report_wrapper.py

---
+++ core/report_wrapper.py
@@ -0,0 +1,6 @@
+# Import trip_worker
+from trip_worker import TripWorker
+
+# In gnerate_report function
+    elif report_type == 'trip':
+        report_handler = TripWorker()  # Add TripWorker for trip planning report
