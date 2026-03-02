9d6de1d600cb323d7ac7ccb11274c4f9318578ee
bernievcooke-cloud <bernievcooke@gmail.com>
2026-03-02 11:01:39 +1100

    Update settings.py to use environment variables

--- Sentinel-Access-V2/config/settings.py
+++ Sentinel-Access-V2/config/settings.py
@@ -10,19 +10,19 @@ BASE_OUTPUT = os.getenv("BASE_OUTPUT_PATH", r"C:\OneDrive\Sentinel-Access-v2\sto
 BASE_OUTPUT_PATH = os.getenv("BASE_OUTPUT_PATH", r"C:\OneDrive\Sentinel-Access-v2\storage\reports")
 
 # GitHub Configuration
-GITHUB_USERNAME=bernievcooke-cloud
-GITHUB_REPO=Sentinel2-Access
-GITHUB_TOKEN=y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
+GITHUB_USERNAME = os.getenv("GITHUB_USERNAME", "")
+GITHUB_REPO = os.getenv("GITHUB_REPO", "")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
 
 # Report Types
 REPORT_TYPES = os.getenv("REPORT_TYPES", "Surf, Sky, Weather").split(",")
 
 # Email Configuration
-SENDER_EMAIL=bernievcooke@gmail.com
-SENDER_PASSWORD=kmoaifjbaufxnksf
-SMTP_SERVER=smtp.gmail.com
-SMTP_PORT=587
+SENDER_EMAIL = os.getenv("SENDER_EMAIL", "")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD", "")
+SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
 
 # Debug mode
-
 DEBUG = os.getenv("DEBUG", "True") == "True"
+
